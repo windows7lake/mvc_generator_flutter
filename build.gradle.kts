e76f1bf81b010cf20537cd33d1cdacf4d59fc0bb
LioLin <windows7lake@gmail.com>
2024-05-16 15:36:25 +0800

    feat: set untilBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("*.*")
+        untilBuild.set("")
         changeNotes.set(
             """
             upgrade intellij build version 
